17ff32011e24d0173cf2eebf43122bbf650cd734
lestradavaz <lestradavaz@outlook.com>
2023-05-30 18:51:24 -0600

    -Se añadieron comentarios -Se mejoro la estructura del codigo

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+/* Elementos HTML */
 const form = document.querySelector('#taskForm');
 const input = document.querySelector('#taskInput');
 const modal = document.querySelector('#modal');
-const list = document.querySelector('#taskList');
+const taskList = document.querySelector('#taskList');
 const label = document.querySelector('.no-content');
 
+/* Clase para las tareas y su informacion */
 class Task {
+    /* Constructor de la clase que solicita una descripcion de la tarea y genera la fecha de creacion */
     constructor(desc) {
         this.description = desc;
 
@@ -17,35 +20,26 @@ class Task {
     time;
 }
 
-var tasks = [];
-
-form.addEventListener('submit', () => {
-    event.preventDefault();
-
-    var list = new TaskList();
-
-    input.value.length > 0
-        ? list.addTask(input.value)
-        : alert('Ingresa una tarea');
-
-    tasks.length() > 0
-        ? (label.style.display = 'none')
-        : (label.style.display = 'block');
-});
-
+/* Clase de la lista de tareas con sus funciones  */
 class TaskList {
+    tasks = [];
+
+    /* Funcion para añadir las tareas a la lista */
     addTask(string) {
         var task = new Task(string);
 
-        tasks.push(task);
+        this.tasks.push(task);
+
+        input.value = '';
 
         this.updateTasks();
     }
 
+    /* Funcion para actualizar los elementos HTML de la lista */
     updateTasks() {
         let string = '';
         let i = 1;
-        tasks.forEach((task) => {
+        this.tasks.forEach((task) => {
             string += `
             <li class="task">
                 <span class="taskIterator">
@@ -63,6 +57,23 @@ class TaskList {
             i++;
         });
 
-        list.innerHTML = string;
+        taskList.innerHTML = string;
     }
 }
+
+/* Inicializacion de la lista de tareas */
+var list = new TaskList();
+
+/* Listener que se activa al hacer el submit del form, con condicionales que verifican el contenido del INPUT */
+form.addEventListener('submit', (e) => {
+    /* Funcion que nos permite evitar que el sumbit del formulario recargue la pagina */
+    e.preventDefault();
+
+    input.value.length > 0
+        ? list.addTask(input.value)
+        : alert('Ingresa una tarea');
+
+    list.tasks.length > 0
+        ? (label.style.display = 'none')
+        : (label.style.display = 'block');
+});
